5a901ca8f0c828d0ea1a66335fbb45feeff4edfc
Pratik Bhumkar <prateekbhumkar@gmail.com>
2021-03-05 12:58:29 +1100

    Exceptions,Refactoring and Adding integration tests
    Exception to handle:
    - Invalid data sent
    - Invalid source value
     Moving MovieComparer to its file.
    Fixed failing test cases

---
+++ webjetbackendapi/Exceptions/InvalidDataException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace webjetbackendapi.Exceptions
+{
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ webjetbackendapi/Extensions/MovieComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using webjetbackendapi.Models;
+
+namespace webjetbackendapi.Extensions
+{
+    public class MovieComparer : IEqualityComparer<Movie>
+    {
+        public bool Equals(Movie x, Movie y)
+        {
+            return y != null && x != null && x.Title.Equals(y.Title);
+        }
+        public int GetHashCode(Movie obj)
+        {
+            return obj.Title.GetHashCode();
+        }
+    }
+}
